9f5673b735ff20b834b308f7e9c61bd565f9d766
Adriel <adriel.fsantos@outlook.com>
2022-12-01 23:37:19 -0300

    Move tags link de estilo para _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,18 +9,12 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Great+Vibes&family=Outfit:wght@300;400;500;600&display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <GlobalStyle />
-      <Header />
       <main>
         <Container>
           <Component {...pageProps} />
