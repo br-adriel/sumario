fd3c56ebe2010e1c9701725b5b8ffd0963ac7420
Adriel <adriel.fsantos@outlook.com>
2022-12-01 21:47:03 -0300

    Adiciona componente de estilo global

---
+++ components/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Outfit', sans-serif;
+    background: #fdfeff;
+  }
+
+  #__next {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+`;
+
+export default GlobalStyle;
