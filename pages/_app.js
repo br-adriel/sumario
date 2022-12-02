b0649b55b98994db7751e601cac996f1947d6560
Adriel <adriel.fsantos@outlook.com>
2022-12-01 21:47:34 -0300

    Implementa layout base

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import Header from '../components/Header';
+import GlobalStyle from '../components/GlobalStyle';
+import Head from 'next/head';
+import Footer from '../components/Footer';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Container>
-    <Component {...pageProps} />
-  </Container>
+  return (
+    <>
+      <Head>
+        <meta charset='UTF-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Great+Vibes&family=Outfit:wght@300;400;500;600&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <GlobalStyle />
+      <Header />
+      <main>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
