5f841b97e0d15f20f1ee2827f0070338e97288c2
Adriel <adriel.fsantos@outlook.com>
2022-12-04 17:03:22 -0300

    Implementa página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
-import { Row, Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
+import useSWR from 'swr';
+import BooksTable from '../components/BooksTable';
 import Header from '../components/Header';
 import SearchForm from '../components/SearchForm';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [urlQuery, setUrlQuery] = useState('');
+  const [booksData, setBooksData] = useState({});
+
+  const { data, error } = useSWR(urlQuery, async (url) => {
+    if (url === '' || error) return { docs: [] };
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+  });
+
+  useEffect(() => {
+    setBooksData(data);
+  }, [data]);
+
+  const searchFormSubmit = (e) => {
+    e.preventDefault();
+    const busca = e.target['busca'].value.trim();
+    const queryString = `http://openlibrary.org/search.json?q=${busca}&fields=key,author_name,first_publish_year,title`;
+    setUrlQuery(queryString);
+  };
+
   return (
     <Row>
       <Col xs='12' md='4'>
         <Header />
-        <SearchForm onSubmit={() => {}} />
+        <SearchForm onSubmit={searchFormSubmit} />
       </Col>
-      <Col xs='12' md='8'></Col>
+      <Col xs='12' md='8'>
+        <BooksTable books={booksData} />
+      </Col>
     </Row>
   );
 };
