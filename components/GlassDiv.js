cde90070d5653c3cfc2081c8acbb0186fe13c8dc
Adriel <adriel.fsantos@outlook.com>
2022-12-08 00:19:21 -0300

    Adiciona componente GlassDiv

---
+++ components/GlassDiv.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const GlassDiv = styled.div`
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 6px 0 rgba(31, 38, 135, 0.2);
+  backdrop-filter: blur(7px);
+  -webkit-backdrop-filter: blur(7px);
+  border-radius: 5px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 8px;
+`;
+
+export default GlassDiv;
