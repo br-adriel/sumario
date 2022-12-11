a791bff1fc99a5cdf083dd341956ee9db897c161
Adriel <adriel.fsantos@outlook.com>
2022-12-11 11:43:15 -0300

    Adiciona favicon

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,28 @@ export default function MyApp({ Component, pageProps }) {
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
+
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='msapplication-config' content='/browserconfig.xml' />
+        <meta name='msapplication-TileColor' content='#fdfeff' />
+        <meta name='theme-color' content='#fdfeff' />
       </Head>
       <GlobalStyle />
       <main>
